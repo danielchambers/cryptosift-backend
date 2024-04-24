9b0532cee166b6a60c8391cb13cb7469dd26a694
Daniel Chambers <code@danielchambe.rs>
2024-04-23 22:34:09 -0700

    corrected package import

--- app/celery_app/celery.py
+++ app/celery_app/celery.py
@@ -1,5 +1,5 @@
 # app/celery_app/celery.py
 from app.celery_app.celery_instance import app
-from app.celery_app.tasks import user_tasks
+from app.celery_app.tasks import news_tasks
 
 app.autodiscover_tasks(["app.celery_app.tasks"])
