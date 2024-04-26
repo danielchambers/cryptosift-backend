c96a6a93f71e8894fe9a359b8e5428ff7585de32
Daniel Chambers <code@danielchambe.rs>
2024-04-25 18:03:46 -0700

    updated import to current function name (function name changed)

--- app/celery_app/tasks/news_tasks.py
+++ app/celery_app/tasks/news_tasks.py
@@ -3,7 +3,7 @@ import asyncio
 import tornado.gen
 from app.celery_app.celery_instance import app
 from app.redis.redis_instance import get_redis_client
-from .news import main
+from .news import fetch_and_filter_articles
 
 redis_client = get_redis_client()
 
@@ -17,7 +17,41 @@ def async_add(x, y):
 
 
 @app.task
+def broadcast_cardano_message():
+    feed_urls = [
+        "https://newsbtc.com/analysis/ada/feed/",
+        "https://newsbtc.com/news/cardano/feed/",
+        "https://cointelegraph.com/rss/tag/cardano/",
+        "https://cryptoslate.com/news/cardano/feed/",
+        "https://dailycoin.com/cardano-ada/feed/",
+        "https://zycrypto.com/tag/cardano/feed/",
+        "https://zycrypto.com/tag/adausd/feed/",
+        "https://zycrypto.com/tag/ada/feed/",
+        "https://watcher.guru/news/category/cardano/feed/",
+    ]
+    keywords = ["cardano", "hoskinson", "ada", "iohk", "iog", "$ada"]
+    articles = asyncio.run(fetch_and_filter_articles(feed_urls, keywords))
+    message_json = json.dumps(articles)
+    redis_client.publish("notification_channel", message_json)
+
+
+@app.task
 def broadcast_message():
-    articles = asyncio.run(main())
+    feed_urls = [
+        "https://coindesk.com/arc/outboundfeeds/rss/",
+        "https://cryptonews.com/news/feed/",
+        "https://beincrypto.com/feed/",
+        "https://crypto.news/feed/",
+        "https://bitcoinist.com/feed/",
+        "https://cryptopotato.com/feed/",
+        "https://cryptobriefing.com/feed/",
+        "https://dailyhodl.com/feed/",
+        "https://news.bitcoin.com/feed/",
+        "https://forkast.news/feed/",
+        "https://ambcrypto.com/feed/",
+        "https://cryptoglobe.com/rss/feed.xml",
+    ]
+    keywords = ["cardano", "hoskinson", "ada", "iohk", "iog", "$ada"]
+    articles = asyncio.run(fetch_and_filter_articles(feed_urls, keywords))
     message_json = json.dumps(articles)
     redis_client.publish("notification_channel", message_json)
