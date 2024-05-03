c06edfdddfa5d4a95f119d92895d13be0cf2bcb7
Daniel Chambers <code@danielchambe.rs>
2024-05-03 00:20:45 -0700

    updated package for allowing discord task importing

--- app/celery_app/tasks/__init__.py
+++ app/celery_app/tasks/__init__.py
@@ -1,3 +1,4 @@
 from .news_tasks import async_add, broadcast_message
+from .discord_tasks import broadcast_discord_message
 
-__all__ = ["async_add", "broadcast_message"]
+__all__ = ["async_add", "broadcast_message", "broadcast_discord_message"]
