c4210219c37572c7e9e2e48886c96623f9c69dcf
Daniel Chambers <code@danielchambe.rs>
2024-05-08 16:38:23 -0700

    added youtube function for imports

--- app/celery_app/tasks/__init__.py
+++ app/celery_app/tasks/__init__.py
@@ -1,4 +1,5 @@
 from .news_tasks import broadcast_news_data
 from .discord_tasks import broadcast_discord_data
+from .youtube_tasks import broadcast_youtube_data
 
-__all__ = ["broadcast_news_data", "broadcast_discord_data"]
+__all__ = ["broadcast_news_data", "broadcast_discord_data", "broadcast_youtube_data"]
